43036975b53fcbdb78a1136b7ce154ef8893e02e
Sidharth <sidbangera.comms@gmail.com>
2026-05-01 20:04:52 +0530

    feat: create partner_request schema

---
+++ backend/src/db/schemas/partner_request.js
@@ -0,0 +1,27 @@
+import { pgTable as table } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { timestamps } from "./timestamps.js";
+
+// import { eventPostsTable } from "./event_post.js"; will need if eventId is used
+import { usersTable } from "./users.js";
+
+export const partnerRequestsTable = table("partner_request", {
+  requestId: t.integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: t.integer().notNull().references(() => usersTable.userId),
+  
+  //optional: not sure if event id is always required, events from outside the site can be present
+  //eventId: t.integer().references(() => eventPostsTable.eventId),
+  
+  title: t.varchar().notNull(),
+  description: t.text().notNull(),
+  skills: t.varchar().array(),
+  membersRequired: t.integer().notNull(),
+  images: t.varchar(),
+  isCompleted: t.boolean().default(false).notNull(),
+  ...timestamps
+  }, (table) => [
+  // Optional: Add a check so people don't ask for 0 or negative team members
+  t.check('members_required_check', sql`${table.membersRequired} > 0`),
+  t.check('title_check', sql`length(${table.title}) >= 5 AND length(${table.title}) <= 100`)
+]);
