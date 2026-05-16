fe65d397b8e75eda4fa4bf348cdc024b624f173d
siddesh <siddeshpoojary2004@gmail.com>
2026-05-16 16:40:03 +0530

    feat:add partner request

--- backend/src/db/schemas/partners.shema.js
+++ backend/src/db/schemas/partners.shema.js
@@ -1,20 +1,21 @@
 import { pgTable as table } from "drizzle-orm/pg-core";
 import * as t from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
-import { timestamps } from "./timestamps.js";
+import { timestamps } from "./timestamps.schema.js";
 
-import { usersTable } from "./users.js";
+import { usersTable } from "./users.schema.js";
 
 export const partnerRequestsTable = table("partner_request", {
-  requestId: t.integer().primaryKey().generatedAlwaysAsIdentity(),
+  partnerRequestId: t.integer().primaryKey().generatedAlwaysAsIdentity(),
   userId: t.integer().notNull().references(() => usersTable.userId),
-  
   title: t.varchar().notNull(),
   description: t.text().notNull(),
-  skills: t.varchar().array(),
+  skills: t.varchar().array().notNull(),
   membersRequired: t.integer().notNull(),
-  images: t.varchar(),
-  isCompleted: t.boolean().default(false).notNull(),
+  imageLink: t.varchar(),
+  isCompleted: t.boolean().default(false),
+  contactDetails: t.varchar().notNull(),
+  lastDate: t.date(),
   ...timestamps
   }, (table) => [
   // Optional: Add a check so people don't ask for 0 or negative team members
